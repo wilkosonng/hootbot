ca9c4e4ebb605b0cb9aa4acb86df66af0d1e00fd
Wilson Kong <wilkosonng@gmail.com>
2023-08-13 02:21:27 -0400

    Removed extraneous logging

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -42,8 +42,6 @@ function uploadResult(database, title, channel, result) {
 	try {
 		(async () => {
 			// Attempts to add the result to the database
-			console.log(result[0]);
-			console.log(Number(result[0][0]));
 			await set(ref(database, `results/${channel}/${(Date.now() / 1000) | 0}`), {
 				set: title,
 				result: result.map((e) => ({
